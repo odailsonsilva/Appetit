d04e80e0ac9d53140b8348b3eed0f70e87bc6a4a
odailsonsilva <odafreelancerti@gmail.com>
2020-10-13 08:18:15 -0300

    Feat: Page Login - 2

---
+++ src/pages/OrderList/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.css'
+
+function OrderList() {
+  return (
+    <div>
+      <h1>Lista de Pedidos</h1>
+    </div>
+  )
+}
+
+export default OrderList;
